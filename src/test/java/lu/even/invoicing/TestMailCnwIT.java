47e1e21f4459dde5e6be0d5d729af8fda5eb2afc
Gérald Even <gerald.even@arhs-cube.com>
2023-10-30 14:03:06 +0100

    fix test issue, beautify invoice

--- src/test/java/lu/even/invoicing/TestMailCnwIT.java
+++ src/test/java/lu/even/invoicing/TestMailCnwIT.java
@@ -3,21 +3,22 @@ package lu.even.invoicing;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.apache.commons.io.FileUtils;
-import org.apache.poi.util.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.ActiveProfiles;
+
 import java.io.File;
 import java.io.IOException;
 
 @SpringBootTest
-@Profile("cnw")
-public class MailTest {
+@ActiveProfiles("cnw")
+public class TestMailCnwIT {
     @Autowired
     JavaMailSender mailSender;
+
     @Test
     public void testMail() throws MessagingException, IOException {
         File htmlFile = new File("target/notVat.html");
@@ -27,8 +28,8 @@ public class MailTest {
         helper.setFrom("bureau@cnw.lu");
         helper.setTo("gerald.even@gmail.com");
         helper.setSubject("Votre facture: dummy");
-        helper.setText(FileUtils.readFileToString(htmlFile,"UTF-8"),true);
-        helper.addAttachment("file.pdf",pdfFile);
+        helper.setText(FileUtils.readFileToString(htmlFile, "UTF-8"), true);
+        helper.addAttachment("file.pdf", pdfFile);
         mailSender.send(message);
     }
 }
